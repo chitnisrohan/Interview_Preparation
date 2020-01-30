6d106f3c11b289681226cc97410b106a3fb90bf3
Rohan Chitnis <rohanchitnis@Rohans-MacBook.local>
2020-01-30 00:02:27 -0800

    path sum

--- Leetcode/PathSum2.java
+++ Leetcode/PathSum2.java
@@ -1,10 +1,23 @@
-    
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum2 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> result = new ArrayList();
         getPaths(root, sum, result, 0, new ArrayList());
         return result;
     }
-    
+
     private void getPaths(TreeNode root, int sum, List<List<Integer>> result, int currentSum, List<Integer> currentPath) {
         if (root == null) {
             return;
@@ -14,7 +27,8 @@
             result.add(new ArrayList<>(currentPath));
         } else {
             getPaths(root.left, sum, result, currentSum + root.val, currentPath);
-            getPaths(root.right, sum, result, currentSum + root.val, currentPath);            
+            getPaths(root.right, sum, result, currentSum + root.val, currentPath);
         }
         currentPath.remove(currentPath.size() - 1);
     }
+}
